133d5dbc5a665192c17f3065e38960d31d7039b8
Raimi Solorzano <s0557978@htw-berlin.de>
2020-12-10 22:19:45 +0100

    Use typescript - use google style with gts - format on save

---
+++ src/ts/main.ts
@@ -0,0 +1,30 @@
+import * as L from 'leaflet';
+import '../../static/css/main.css';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+const DefaultIcon = L.icon({iconUrl: icon, shadowUrl: iconShadow});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const MAP_ID = 'mapid';
+
+function map_component() {
+  const element = document.createElement('div');
+  element.id = MAP_ID;
+  return element;
+}
+
+document.body.appendChild(map_component());
+
+const map = L.map(MAP_ID).setView(
+  [14.690828817091425, -16.184299667446997],
+  13
+);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+L.marker([14.690828817091425, -16.184299667446997])
+  .addTo(map)
+  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+  .openPopup();
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
-var path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
-    mode: 'development',
-    entry: './src/js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "js/bundle.js",
-        publicPath: "/dist" //emulates the public path for webpack-dev-server
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Communities',
-        }),
-
+  mode: 'development',
+  entry: './src/ts/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+    publicPath: '/dist', //emulates the public path for webpack-dev-server
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Communities',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    devServer: {
-        index: 'index.html',
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        hot: true, //Hot module replacement
-        port: 9000,
-        writeToDisk: true,
-        open: 'firefox', //open in chrome
-        publicPath: "/dist"
-    }
+  },
+  devServer: {
+    index: 'index.html',
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true, //Hot module replacement
+    port: 9000,
+    writeToDisk: true,
+    open: 'firefox', //open in chrome
+    publicPath: '/dist',
+  },
 };
